0f553b25cc63d3a884ab6d9df38b262e9b83f56e
Dimitar Kralev <mitko.kralev@abv.bg>
2023-07-20 16:01:38 +0300

    React setup

---
+++ Budgy-Client/budgy/src/components/Home/Home.js
@@ -0,0 +1,8 @@
+export const Home = () => {
+    return (
+        <div>
+            <h1>WELCOME TO BUDGY</h1>
+            <h5>Control your budget and spendings in a inovative way !</h5>
+        </div>
+    );
+};
